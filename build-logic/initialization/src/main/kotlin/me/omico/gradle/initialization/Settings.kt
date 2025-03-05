4b50d5fbfa70de4b54f467bdd836a9287b686835
Omico <me@omico.icu>
2025-03-05 12:10:40 -0800

    Update

--- build-logic/initialization/src/main/kotlin/me/omico/gradle/initialization/Settings.kt
+++ build-logic/initialization/src/main/kotlin/me/omico/gradle/initialization/Settings.kt
@@ -4,11 +4,37 @@ import org.gradle.api.initialization.Settings
 import java.io.File
 
 internal fun Settings.includeAllSubprojectModules(projectName: String): Unit =
-    rootDir.resolve(projectName).walk()
+    collectSubprojectModules(projectName).forEach { moduleDirectory ->
+        val moduleName = moduleDirectory.relativeTo(rootDir).path.replace(File.separator, "-")
+        include(":$moduleName")
+        project(":$moduleName").projectDir = moduleDirectory
+    }
+
+private fun Settings.collectSubprojectModules(projectName: String): Set<File> =
+    mutableSetOf<File>().apply {
+        val projectDirectory = rootDir.resolve(projectName)
+        projectDirectory.mkdirs()
+        collectSubprojectModules(projectDirectory)
+    }
+
+private fun MutableSet<File>.collectSubprojectModules(parentDirectory: File): Unit =
+    parentDirectory.listFiles()
+        .asSequence()
+        // Only include directories.
         .filter(File::isDirectory)
-        .filter { it.resolve("build.gradle.kts").exists() }
-        .forEach { moduleDirectory ->
-            val moduleName = moduleDirectory.relativeTo(rootDir).path.replace(File.separator, "-")
-            include(":$moduleName")
-            project(":$moduleName").projectDir = moduleDirectory
+        // Exclude the `<parentDirectory>/<ignoredName>` directory if `<parentDirectory>/build.gradle.kts` exists.
+        .filterNot { it.name in ignoredSubprojectDirectoryNames && it.resolveSibling("build.gradle.kts").exists() }
+        .forEach { file ->
+            // Find any directory that contains a `build.gradle.kts` file.
+            if (file.resolve("build.gradle.kts").exists()) this += file
+            collectSubprojectModules(file)
         }
+
+private val ignoredSubprojectDirectoryNames: Set<String> =
+    setOf(
+        ".kotlin",
+        ".gradle",
+        ".idea",
+        "src",
+        "build",
+    )
